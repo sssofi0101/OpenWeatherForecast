9377bcec6f64c4949ced272ddef7c1970aa5b80b
Sofya Sokhina <55346176+sssofi0101@users.noreply.github.com>
2023-11-16 14:10:53 +0300

    Реализовано переключение по нижнему меню

--- app/src/main/java/com/example/openweatherforecast/presentation/MainActivity.kt
+++ app/src/main/java/com/example/openweatherforecast/presentation/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.openweatherforecast.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.openweatherforecast.R
 import com.example.openweatherforecast.data.repository.RepositoryImpl
 import com.example.openweatherforecast.data.retrofit.RetrofitImpl
+import com.example.openweatherforecast.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -14,10 +17,46 @@ import okhttp3.Dispatcher
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         getF()
+
+        val navView: BottomNavigationView = binding.bottomNavigationView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        navView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_weather -> {
+                    navController.navigate(R.id.weatherFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_air -> {
+                    navController.navigate(R.id.airFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_cities -> {
+                    navController.navigate(R.id.citiesFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_map -> {
+                    navController.navigate(R.id.mapFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_settings -> {
+                    navController.navigate(R.id.settingsFragment)
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> {true}
+            }
+
+        }
     }
 
     fun getF(){
